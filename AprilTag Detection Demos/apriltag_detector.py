207267717d876e910b6b8f7c4292a27ada982515
Zachary Schallenberger <zapps98@gmail.com>
2021-06-01 10:13:12 -0500

    AprilTag Demos
    Added our first two AprilTag detection programs we created

---
+++ AprilTag Detection Demos/apriltag_detector.py
@@ -0,0 +1,20 @@
+from picamera import PiCamera
+from time import sleep
+import cv2
+import numpy as np
+from apriltag import apriltag
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/Desktop/image.jpg')
+camera.stop_preview()
+
+imagepath = '/home/pi/Desktop/image.jpg'
+image = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
+detector = apriltag("tagStandard41h12")
+
+detections = detector.detect(image)
+
+print(detections)
