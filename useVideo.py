9a12386e8a9ff06920f5ed8cb62709af6a2d6955
Zachary Schallenberger <zapps98@gmail.com>
2021-06-03 11:35:36 -0500

    Added useVideo.py
    from PS-Drone

---
+++ useVideo.py
@@ -0,0 +1,46 @@
+#########
+# useVideo.py
+# This program is part of the online PS-Drone-API-tutorial on www.playsheep.de/drone.
+# It shows the usage of the video-function of a Parrot AR.Drone 2.0 using the PS-Drone-API.
+# The drone will stay on the ground.
+# Dependencies: a POSIX OS, openCV2, PS-Drone-API 2.0 beta or higher.
+# (w) J. Philipp de Graaff, www.playsheep.de, 2016
+##########
+# LICENCE:
+#   Artistic License 2.0 as seen on http://opensource.org/licenses/artistic-license-2.0 (retrieved December 2014)
+#   Visit www.playsheep.de/drone or see the PS-Drone-API-documentation for an abstract from the Artistic License 2.0.
+###########
+
+##### Suggested clean drone startup sequence #####
+import time, sys
+import ps_drone													# Import PS-Drone-API
+import cv2														# Import OpenCV
+
+drone = ps_drone.Drone()										# Start using drone
+drone.startup()													# Connects to drone and starts subprocesses
+
+drone.reset()													# Sets drone's status to good (LEDs turn green when red)
+while (drone.getBattery()[0]==-1):	time.sleep(0.1)				# Waits until drone has done its reset
+print "Battery: "+str(drone.getBattery()[0])+"%  "+str(drone.getBattery()[1])	# Gives a battery-status
+drone.useDemoMode(True)											# Just give me 15 basic dataset per second (is default anyway)
+
+##### Mainprogram begin #####
+drone.setConfigAllID()				# Go to multiconfiguration-mode
+drone.sdVideo()						# Choose lower resolution (hdVideo() for...well, guess it)
+drone.frontCam()					# Choose front view
+CDC = drone.ConfigDataCount
+while CDC == drone.ConfigDataCount:	time.sleep(0.0001)	# Wait until it is done (after resync is done)
+drone.startVideo()					# Start video-function
+
+##### And action !
+IMC = 	 drone.VideoImageCount		# Number of encoded videoframes
+stop =	 False
+while not stop:
+	while drone.VideoImageCount==IMC: time.sleep(0.01)	# Wait until the next video-frame
+	IMC = drone.VideoImageCount
+	key = drone.getKey()
+	if key:		stop = True
+	img  = drone.VideoImage					# Copy video-image
+	pImg = cv2.resize(img,(400,100))		# Process video-image
+	cv2.imshow('Drones video',pImg)			# Show processed video-image
+	cv2.waitKey(1)							# OpenCV for Linux has a bug and needs this line
