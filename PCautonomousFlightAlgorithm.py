fcdfc2d45599f67f65dc5e0466247fd4949bddf5
Zachary Schallenberger <zapps98@gmail.com>
2021-06-21 14:07:33 -0500

    Update PCautonomousFlightAlgorithm.py

--- PCautonomousFlightAlgorithm.py
+++ PCautonomousFlightAlgorithm.py
@@ -2,7 +2,16 @@
 #This is a simulated program
 import time
 import os
+import board
+import busio
+import adafruit_lsm303_accel
+import adafruit_lsm303dlh_mag
+
+
 os.system('clear')
+i2c = busio.I2C(board.SCL, board.SDA)
+mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
+accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
 #Pseduocode for alignmentAlgorithm()
 def alignmentAlgorithm():
     print("Running alignment algorithm!")
@@ -38,6 +47,7 @@ while (timeAllotted > time.time()):
         else:
             os.system('clear')
             time_convert(time.time())
+            print("Magnetometer (micro-Teslas)): X=%0.3f Y=%0.3f Z=%0.3f"%mag.magnetic)
 print("While loop has concluded")
 finish = time.time()
 time_convert(finish - start_time)
