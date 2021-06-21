274c2b11b155107c696bb9b4cd81548cd5631e55
Zachary Schallenberger <zapps98@gmail.com>
2021-06-21 16:58:55 -0500

    Added historicalNavData program
    This program is designed to write to a temporary text file all of the historical data from a flight

---
+++ historicalNavData.py
@@ -0,0 +1,34 @@
+import os
+import time
+import board
+import busio
+import adafruit_lsm303_accel
+import adafruit_lsm303dlh_mag
+from pynput import keyboard                                                      # Import PS-Drone-API
+import signal
+
+i2c = busio.I2C(board.SCL, board.SDA)
+mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
+accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
+
+home_dir = os.path.expanduser('~')
+repository_dir = os.path.join(home_dir, 'Desktop/AU-REU-21')
+
+
+end = False
+
+
+def shutdown_gracefully(signal, frame):
+    exit()
+
+signal.signal(signal.SIGQUIT, exit_gracefully)
+
+with open(os.path.join(repository_dir, 'temp.txt'),'w') as file:
+    file.write("")
+file.close()
+
+with open(os.path.join(repository_dir, 'temp.txt'),'a') as file:
+    while not end:
+        print("Magnetometer (micro-Teslas)): X=%i Y=%i Z=%i"%(int(mag.magnetic[0]),int(mag.magnetic[1]),int(mag.magnetic[2])))
+        print("Accelerometer (m/s^2): X=%0.3f Y=%0.3f Z=%0.3f"%(accel.acceleration[0],accel.acceleration[1],accel.acceleration[2]))
+file.close()
