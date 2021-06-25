005aea38fd053ded15af4e8eadb8559fbbcf5c0b
Schallenballa <zachietaylor@msn.com>
2021-06-25 15:28:45 -0500

    Update fly_uav.py

--- fly_uav.py
+++ fly_uav.py
@@ -17,13 +17,13 @@ IMAGE_SIZE = (640, 360)
 # X-axis threshold, altitude, and altitude threshold for navigation
 NAV_DATA = (20, 500, 20)
 # X- and Y-axis threshold, altitude and altitude threshold for alignment
-ALIGN_DATA = (10, 10, 500, 20)
+ALIGN_DATA = (30, 30, 1120, 30)
 # Known vlaues for x, y, z-position of each marker and yaw needed for path to
 # next marker. The position of each data tuple corresponds with the marker ID.
-MARKER_DATA = ((x, y, z, yawForNextMarker), (x, y, z, yawForNextMarker)) # TODO: fill in
-
+MARKER_DATA = ((-115, -75, 145, -0.350), ("""x, y, z, yawForNextMarker""")) # TODO: fill in
+drone = ps_drone.Drone()
 # Allows manual override shutdown
-def exit_gracefully(drone, signal, frame):
+def exit_gracefully(signal, frame):
     print("Shutting down")
     drone.shutdown()
 
@@ -43,23 +43,25 @@ def start_drone(drone):
 
     print("Taking off")
     drone.takeoff()
-
+    time.sleep(30)
 # Detects and displays a marker in an image
 def detect(camera, detector):
+    print("Starting detection...")
     ret, img = camera.read()
     image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     detection = detector.detect(image)
-    if detection["margin"] >= 10:
-       rect = detection["lb-rb-rt-lt"].astype(int).reshape((-1, 1, 2))
-       cv2.polylines(img, [rect], True, BLUE, 2)
-       ident = str(detection["id"])
-       pos = detection["center"].astype(int) + (-10, 10)
-       cv2.putText(img, ident, tuple(pos), cv2.FONT_HERSHEY_SIMPLEX, 1, BLUE, 2)
-    cv2.imshow("IMG", img)
-    print(detection)
-
+    if len(detection) != 0:
+        if detection[0]["margin"] >= 10:
+       	    rect = detection[0]["lb-rb-rt-lt"].astype(int).reshape((-1, 1, 2))
+            cv2.polylines(img, [rect], True, BLUE, 2)
+       	    ident = str(detection[0]["id"])
+       	    pos = detection[0]["center"].astype(int) + (-10, 10)
+       	    cv2.putText(img, ident, tuple(pos), cv2.FONT_HERSHEY_SIMPLEX, 1, BLUE, 2)
+            cv2.imshow("IMG", img)
+        print(detection)
+        return detection[0]
+    print("Ending detection...")
     return detection
-
 # Aligns drone with center of marker
 # Quadrants:
 # II    I
@@ -67,13 +69,13 @@ def detect(camera, detector):
 # Successful alignment is determined when the center of the marker is within a 20x20 pixel-box of the detection area's center
 def align(drone, camera, detector):
     # If the y-value of the AprilTag center is in the III or IV quadrant of the detection zone (leaves a +10-pixel buffer)
-    if detect(camera, detector)["center"][1] > IMAGE_SIZE[1] // 2 + ALIGN_DATA[1]: # Move forward
-        drone.moveForward()
+    if detect(camera, detector)["center"][1] > IMAGE_SIZE[1] // 2 + ALIGN_DATA[1]: # Move backward
+        drone.moveBackward()
         while detect(camera, detector)["center"][1] > IMAGE_SIZE[1] // 2 + ALIGN_DATA[1]:
             continue
     # Else if the y-value of the AprilTag center is in the I or II quadrant of the detection zone (leaves a -10-pixel buffer)
-    elif detect(camera, detector)["center"][1] < IMAGE_SIZE[1] // 2 - ALIGN_DATA[1]: # Move backward
-        drone.moveBackward()
+    elif detect(camera, detector)["center"][1] < IMAGE_SIZE[1] // 2 - ALIGN_DATA[1]: # Move forward
+        drone.moveForward()
         while detect(camera, detector)["center"][1] < IMAGE_SIZE[1] // 2 - ALIGN_DATA[1]:
             continue
     drone.stop()
@@ -104,7 +106,7 @@ def orient(drone, camera, detector):
 def marker_navigate(drone, camera, detector):
     drone.moveForward()
     # While the AprilTag center's y-value is outside of the acceptable detection range (but still detected, nevertheless)
-    while detect(camera, detector)["center"][1] > IMAGE_SIZE[1] // 2 + ALIGN_DATA[1]:
+    while detect(camera, detector)["center"][1] < IMAGE_SIZE[1] // 2 - ALIGN_DATA[1]:
         # If the x-value of the AprilTag's center is to the right of the detection center
         if detect(camera, detector)["center"][0] > IMAGE_SIZE[0] // 2 + ALIGN_DATA[0]: # Move right
             drone.moveRight()
@@ -166,17 +168,21 @@ def main():
     drone = ps_drone.Drone()
     start_drone(drone)
 
+    x_pos = drone.NavData["magneto"][0][0]
+    navigate(drone, x_pos, camera, detector)
     marker_navigate(drone, camera, detector)
     align(drone, camera, detector)
-    orient(drone, camera, detector)
-    while not detect(camera, detector)["id"] == LAST_MARKER_ID:
-        x_pos = drone.NavData["magneto"][0][0]
-        navigate(drone, x_pos, camera, detector)
-        marker_navigate(drone, camera, detector)
-        align(drone, camera, detector)
-        orient(drone, camera, detector)
+#     orient(drone, camera, detector)
+#     while not detect(camera, detector)["id"] == LAST_MARKER_ID:
+#         x_pos = drone.NavData["magneto"][0][0]
+#         navigate(drone, x_pos, camera, detector)
+#         marker_navigate(drone, camera, detector)
+#         align(drone, camera, detector)
+#         orient(drone, camera, detector)
 
     print("Landing")
     drone.shutdown()
     cv2.destroyAllWindows()
     print("Done")
+
+main()
