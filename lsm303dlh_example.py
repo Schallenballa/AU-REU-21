e121504bcee130c9ff651cf78db3b583e15a60ab
Nicholas Dunn <44147236+nicholasbdunn@users.noreply.github.com>
2021-06-30 15:49:02 -0500

    Update lsm303dlh_example.py

--- lsm303dlh_example.py
+++ lsm303dlh_example.py
@@ -1,11 +1,13 @@
 # Example usage of the LSM303DLHC accelerometer/magnetometer
 
-from time import sleep
 import board
 import busio
 import adafruit_lsm303_accel
 import adafruit_lsm303dlh_mag
-from pynput import keyboard
+#from pynput import keyboard
+import signal
+import math
+from time import sleep
 
 i2c = busio.I2C(board.SCL, board.SDA)
 mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
@@ -13,16 +15,30 @@ accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
 
 end = False
 
-def on_press(key):
+# def on_press(key):
+#     global end
+#     end = True
+#     return False
+# 
+# listener = keyboard.Listener(on_press=on_press)
+# listener.start()
+
+def exit_gracefully(signal, frame):
+    print("Ending")
     global end
     end = True
-    return False
 
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
+signal.signal(signal.SIGQUIT, exit_gracefully)
 
 while not end:
     print("Acceleration (m/s^2): X=%0.3f Y=%0.3f Z=%0.3f"%accel.acceleration)
     print("Magnetometer (micro-Teslas)): X=%0.3f Y=%0.3f Z=%0.3f"%mag.magnetic)
+    angle = math.degrees(math.atan2(mag.magnetic[1],mag.magnetic[0]))
+    angle-=80
+    if angle<0:
+        angle+=360
+    print("Angle (degrees): "+str(angle))
     print("")
     sleep(0.5)
+    
+print("Done")
